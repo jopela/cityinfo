d8587451cf381c7b738fec36f7dee6ce03d180c0
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2013-12-16 19:56:19 +0000

    adding test case to function

--- cityinfo.py
+++ cityinfo.py
@@ -21,7 +21,19 @@ def main():
             default = sys.stdin
             )
 
+    parser.add_argument(
+            '-t',
+            '--test',
+            help='run the doctest suite and exit',
+            action='store_true'
+            )
+
     args = parser.parse_args()
+
+    if args.test:
+        import doctest
+        doctest.testmod()
+        exit(0)
 
     jsonguide = json.load(args.guide)
     info = cityinfo(jsonguide)
@@ -30,7 +42,17 @@ def main():
 
 
 def cityinfo(jsonguide):
-    """ return the city name and the bounding box, separated by a ';'."""
+    """
+    return the city name and the bounding box, separated by a ';'.
+
+    EXAMPLE
+    =======
+
+    >>> jsonguide = json.load(open('./test/inguide.json','r'))
+    >>> cityinfo(jsonguide)
+    'Bali;-8.04968577,114.3502976,-8.85186802,115.76261798'
+
+    """
     city = jsonguide['Cities'][0]['name']
     bb = ",".join(jsonguide['Cities'][0]['bounding_box'])
     res = ";".join([city,bb])
---
+++ setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from disutils.core import setup
+setup( name='cityinfo', version='0.1.1', py_modules=['cityinfo'])
+
